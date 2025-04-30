ddb440e076fa412114396b5be7f11c71e9ffcf1f
Anthony Callegari <acallega@stevens.edu>
2025-04-30 12:19:34 -0400

    Update pong.vhd

--- Car Game/pong.vhd
+++ Car Game/pong.vhd
@@ -1,40 +1,164 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY leddec16 IS
-	PORT (
-		dig : INOUT STD_LOGIC_VECTOR (2 DOWNTO 0); -- which digit to currently display
-		data : IN sTD_LOGIC_VECTOR (15 DOWNTO 0); -- 16-bit (4-digit) data
-		anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- which anode to turn on
-		seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)); -- segment code for current digit
-END leddec16;
+ENTITY pong IS
+    PORT (
+        clk_in : IN STD_LOGIC; -- system clock
+        VGA_red : OUT STD_LOGIC_VECTOR (3 DOWNTO 0); -- VGA outputs
+        VGA_green : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        VGA_blue : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        VGA_hsync : OUT STD_LOGIC;
+        VGA_vsync : OUT STD_LOGIC;
+        btnl : IN STD_LOGIC;
+        btnr : IN STD_LOGIC;
+        btn0 : IN STD_LOGIC;
+        KB_row : OUT STD_LOGIC_VECTOR (4 DOWNTO 1);
+        KB_col : INOUT STD_LOGIC_VECTOR (4 DOWNTO 1);
+        SEG7_anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- anodes of four 7-seg displays
+        SEG7_seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+    ); 
+END pong;
 
-ARCHITECTURE Behavioral OF leddec16 IS
-	SIGNAL data4 : STD_LOGIC; -- binary value of current digit
+ARCHITECTURE Behavioral OF pong IS
+    SIGNAL pxl_clk : STD_LOGIC := '0'; -- 25 MHz clock to VGA sync module
+    -- internal signals to connect modules
+    SIGNAL S_red, S_green, S_blue : STD_LOGIC; --_VECTOR (3 DOWNTO 0);
+    SIGNAL S_vsync : STD_LOGIC;
+    SIGNAL S_pixel_row, S_pixel_col : STD_LOGIC_VECTOR (10 DOWNTO 0);
+    SIGNAL batpos : STD_LOGIC_VECTOR (10 DOWNTO 0); -- 9 downto 0
+    SIGNAL car2pos : STD_LOGIC_VECTOR (10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(475, 11);-- 9 downto 0
+    SIGNAL count : STD_LOGIC_VECTOR (20 DOWNTO 0);
+    SIGNAL display : std_logic_vector (15 DOWNTO 0); -- value to be displayed
+    SIGNAL led_mpx : STD_LOGIC_VECTOR (2 DOWNTO 0); -- 7-seg multiplexing clock
+    SIGNAL kb_row_signal : STD_LOGIC_VECTOR(4 DOWNTO 1);
+    SIGNAL kb_col_signal : STD_LOGIC_VECTOR(4 DOWNTO 1); 
+    SIGNAL kp_value : std_logic_vector (3 DOWNTO 0);
+    
+    --component for keypad
+--    	COMPONENT keypad IS
+--		PORT (
+--			col : OUT STD_LOGIC_VECTOR (4 DOWNTO 1);
+--			value : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+--			row : IN STD_LOGIC_VECTOR (4 DOWNTO 1)
+
+--		);
+--    end component;
+    
+    COMPONENT bat_n_ball IS
+        PORT (
+            v_sync : IN STD_LOGIC;
+            pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            pixel_col : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            bat_x : IN STD_LOGIC_VECTOR (10 DOWNTO 0);
+            car2_x : IN STD_LOGIC_VECTOR (10 DOWNTO 0);
+            serve : IN STD_LOGIC;
+            red : OUT STD_LOGIC;
+            green : OUT STD_LOGIC;
+            blue : OUT STD_LOGIC;
+            hit_cnt : STD_LOGIC_VECTOR(15 DOWNTO 0)
+        );
+    END COMPONENT;
+    COMPONENT vga_sync IS
+        PORT (
+            pixel_clk : IN STD_LOGIC;
+            red_in    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_in  : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_in   : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            red_out   : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_out : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_out  : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            hsync : OUT STD_LOGIC;
+            vsync : OUT STD_LOGIC;
+            pixel_row : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
+            pixel_col : OUT STD_LOGIC_VECTOR (10 DOWNTO 0)
+        );
+    END COMPONENT;
+    COMPONENT clk_wiz_0 is
+        PORT (
+            clk_in1  : in std_logic;
+            clk_out1 : out std_logic
+        );
+    END COMPONENT;
+    COMPONENT leddec16 IS
+        PORT (
+            dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+            data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+            anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+            seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+        );
+    END COMPONENT; 
+    
 BEGIN
-
-	data4 <= data(0) when dig = "000" else
-	       data(1) when dig = "001" else
-	       data(2) when dig = "010" else
-	       data(3) when dig = "011" else
-	       data(4) when dig = "100" else
-	       data(5) when dig = "101" else
-	       data(6) when dig = "110" else
-	       data(7) when dig = "111" else
-	       '0';
-	
-	
-	seg <= "0000001" WHEN data4 = '0' ELSE -- 0
-	       "1001111" ; 
-	-- Turn on anode of 7-segment display addressed by 3-bit digit selector dig
-	anode <= "11111110" WHEN dig = "000" ELSE -- 0
-             "11111101" WHEN dig = "001" ELSE -- 1
-	         "11111011" WHEN dig = "010" ELSE -- 2
-	         "11110111" WHEN dig = "011" ELSE -- 3
-	         "11101111" WHEN dig = "100" ELSE -- 4
-	         "11011111" WHEN dig = "101" ELSE -- 5 
-	         "10111111" WHEN dig = "110" ELSE -- 6
-	         "01111111" WHEN dig = "111" ELSE -- 7
-	         "11111111";
+    pos : PROCESS (clk_in) is
+    BEGIN
+        if rising_edge(clk_in) then
+            count <= count + 1;
+            IF (btnl = '1' and count = 0 and batpos > 0) THEN
+                batpos <= batpos - 10;
+            ELSIF (btnr = '1' and count = 0 and batpos < 305) THEN
+                batpos <= batpos + 10;
+            END IF;
+            
+            kb_row_signal <= "1101"; -- Only Row 3 low (active low)
+        
+    
+            if (KB_col(1)= '0' and count = 0 and car2pos > 500) then
+                car2pos <= car2pos - 10;
+        elsif (KB_col(2)= '0' and count = 0 and car2pos < 775) then
+                car2pos <= car2pos + 10;
+            end if;
+        end if;
+        
+    END PROCESS;
+    
+    KB_row <= kb_row_signal;
+    led_mpx <= count(19 DOWNTO 17); -- 7-seg multiplexing clock 
+       
+    add_bb : bat_n_ball
+    PORT MAP(--instantiate bat and ball component
+        v_sync => S_vsync, 
+        pixel_row => S_pixel_row, 
+        pixel_col => S_pixel_col, 
+        bat_x => batpos, 
+        serve => btn0, 
+        red => S_red, 
+        green => S_green, 
+        blue => S_blue,
+        hit_cnt => display,
+        car2_x => car2pos
+    );
+    
+    vga_driver : vga_sync
+    PORT MAP(--instantiate vga_sync component
+        pixel_clk => pxl_clk, 
+        red_in => S_red & "000", 
+        green_in => S_green & "000", 
+        blue_in => S_blue & "000", 
+        red_out => VGA_red, 
+        green_out => VGA_green, 
+        blue_out => VGA_blue, 
+        pixel_row => S_pixel_row, 
+        pixel_col => S_pixel_col, 
+        hsync => VGA_hsync, 
+        vsync => S_vsync
+    );
+    VGA_vsync <= S_vsync; --connect output vsync
+   
+        
+    clk_wiz_0_inst : clk_wiz_0
+    port map (
+      clk_in1 => clk_in,
+      clk_out1 => pxl_clk
+    );
+    led1 : leddec16
+    PORT MAP(
+      dig => led_mpx, data => display, 
+      anode => SEG7_anode, seg => SEG7_seg
+    );
+--    kp : keypad
+--    PORT MAP(
+--      row => kb_row, col => kb_col, value => kp_value
+--    );
 END Behavioral;
-
